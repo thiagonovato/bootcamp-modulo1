06d2e7b920ad840abfdf5e21903507e316806117
Thiago Novato <thiagonovato@gmail.com>
2019-02-18 16:40:32 -0300

    Implementando middleware.

--- index.js
+++ index.js
@@ -5,9 +5,26 @@ const express = require("express");
 // Criar um servidor do express
 const app = express();
 
+// Criação de um Interceptador
+// O parãmetro NEXT, tanto na primeira linha quanto na última
+// não deixa a aplicação travar, ou seja, o middleware roda e continua executando
+const logMiddleware = (req, res, next) => {
+  console.log(
+    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
+  );
+  req.appName = "GoNode";
+  // Deve-se colocar esta linha para "ativar" o next
+  return next();
+};
+
 // Rota /
+// Há duas formas de fazer a aplicação usar o middleware, sendo uam delas passando
+// app.get("/", logMiddleware, (req, res) => {
+// a variável como parâmetro, ou usando a linha abaixo:
+app.use(logMiddleware);
+
 app.get("/", (req, res) => {
-  return res.send(`Bem-vindo, ${req.query.name}`);
+  return res.send(`Bem-vindo ao ${req.appName}, ${req.query.name}`);
 });
 
 // Rota /login
